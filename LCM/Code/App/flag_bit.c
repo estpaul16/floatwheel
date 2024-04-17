766e812eaaf45d445260fe19a21fb700a821d792
estpaul16 <42625205+estpaul16@users.noreply.github.com>
2024-04-16 20:20:30 -0500

    grouped all buzzer related tags together for better readability

--- LCM/Code/App/flag_bit.c
+++ LCM/Code/App/flag_bit.c
@@ -30,48 +30,51 @@ uint8_t WS2812_Display_Flag = 0;
 */
 uint8_t WS2812_Flag = 0;
 /*
-	Power_Display_Flag = 0; ¸ÕÉÏµç
-	Power_Display_Flag = 1£» 4.08V~4.2V 	10¸ö°×µÆ
-	Power_Display_Flag = 2£» 4.05V~4.08V 9¸ö°×µÆ
-	Power_Display_Flag = 3£» 3.96V~4.05V 8¸ö°×µÆ
-	Power_Display_Flag = 4£» 3.87V~3.96V 7¸ö°×µÆ
-	Power_Display_Flag = 5£» 3.78V~3.87V 7¸ö°×µÆ
-	Power_Display_Flag = 6£» 3.70V~3.78V 5¸ö°×µÆ
-	Power_Display_Flag = 7£» 3.62V~3.70V 4¸ö°×µÆ
-	Power_Display_Flag = 8£» 3.50V~3.62V 3¸ö°×µÆ
-	Power_Display_Flag = 9£» 3.35V~3.50V 2¸öºìµÆ
-	Power_Display_Flag = 10; 2.80V~3.35V 1¸öºìµÆ
+	Power_Display_Flag = 0; åˆšä¸Šç”µ
+	Power_Display_Flag = 1ï¼› 4.08V~4.2V  10ä¸ªç™½ç¯
+	Power_Display_Flag = 2ï¼› 4.05V~4.08V 9ä¸ªç™½ç¯
+	Power_Display_Flag = 3ï¼› 3.96V~4.05V 8ä¸ªç™½ç¯
+	Power_Display_Flag = 4ï¼› 3.87V~3.96V 7ä¸ªç™½ç¯
+	Power_Display_Flag = 5ï¼› 3.78V~3.87V 7ä¸ªç™½ç¯
+	Power_Display_Flag = 6ï¼› 3.70V~3.78V 5ä¸ªç™½ç¯
+	Power_Display_Flag = 7ï¼› 3.62V~3.70V 4ä¸ªç™½ç¯
+	Power_Display_Flag = 8ï¼› 3.50V~3.62V 3ä¸ªç™½ç¯
+	Power_Display_Flag = 9ï¼› 3.35V~3.50V 2ä¸ªçº¢ç¯
+	Power_Display_Flag = 10; 2.80V~3.35V 1ä¸ªçº¢ç¯
 */
 uint8_t Power_Display_Flag = 0;
-/*
-	Buzzer_Flag = 0; ¸ÕÉÏµç
-	Buzzer_Flag = 1£»·äÃùÆ÷²»Ïì
-	Buzzer_Flag = 2£»·äÃùÆ÷Ïì
-*/
-uint8_t Buzzer_Flag = 0;
 /*
 	Usart_Flag = Vesc_Data_Ready
 */
 uint8_t Vesc_Data_Ready = 0;
-
 /*
-	·äÃùÆ÷ÏìµÄÊ±¼ä
+	Buzzer_Flag = 0; åˆšä¸Šç”µ
+	Buzzer_Flag = 1ï¼›èœ‚é¸£å™¨ä¸å“
+	Buzzer_Flag = 2ï¼›èœ‚é¸£å™¨å“
+*/
+uint8_t Buzzer_Flag = 0;
+/*
+	èœ‚é¸£å™¨å“çš„æ—¶é—´
 */
 uint16_t Buzzer_Time = 0;
 /*
-	³äµç¼ÆÊ±
+	 Buzzer frequency in BPM, beats per minute, ranging from 60 to 180, corresponding to a duty cycle of 70 to 100)
+*/
+uint8_t Buzzer_Frequency = 0;
+/*
+	å……ç”µè®¡æ—¶
 */
 uint16_t Charge_Time = 0;
 /*
-	ÕÕÃ÷µÆ
+	ç…§æ˜ç¯
 */
 uint16_t Flashlight_Time = 0;
 /*
-	¿ª»úÊ±¼ä
+	å¼€æœºæ—¶é—´
 */
 uint16_t Power_Time = 0;
 /*
-	´®¿ÚÍ¨ĞÅ
+	ä¸²å£é€šä¿¡
 */
 uint16_t Usart_Time = 0;
 /*
@@ -103,13 +106,9 @@ float Charge_Voltage = 0;
 */
 uint8_t Gear_Position = 0;
 /*
-	WS2812Á¿¶È
+	WS2812é‡åº¦
 */
 uint8_t WS2812_Measure = 0;
-/*
-	 Buzzer frequency in BPM, beats per minute, ranging from 60 to 180, corresponding to a duty cycle of 70 to 100)
-*/
-uint8_t Buzzer_Frequency = 0;
 /*
 	 When the button is double-clicked without pressing it, the headlight stays on for 3 seconds.
 */
